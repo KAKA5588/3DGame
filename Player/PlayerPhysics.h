9700abd5145a11bd11f145d1b667eac06ee2edd7
ki582612@gmail.com <ki582612@gmail.com>
2026-02-17 12:11:19 +0900

    2/17 更新

---
+++ Player/PlayerPhysics.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "../Vec3/Vec3.h"   // Åö í«â¡
+
+class Player;
+class Stage;
+
+class PlayerPhysics
+{
+public:
+    void Update(Player& player, Stage* stage, float dt);
+
+private:
+    float radius = 5.0f;
+
+    void ApplyGravity(Player& player, float dt);
+    void ResolveFloor(Player& player, Stage* stage, Vec3& nextPos);
+    void ResolveWall(Player& player, Stage* stage, Vec3& nextPos);
+};
